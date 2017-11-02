36988f0d18afc69c0935057ee67323d169d7f4af
Joker1163 <slepushovii@gmail.com>
2017-11-02 20:52:07 +0400

    initial commit

--- src/Lunchroom/InternetOrder.java
+++ src/Lunchroom/InternetOrder.java
@@ -1,7 +1,32 @@
 package Lunchroom;
 
-public class InternetOrder {
-    private int size;
+public class InternetOrder implements Order {
+    private int size=0;
+    private ListNode head=null;
+    private ListNode tail=null;
     private MenuItem[] items;
+    private Customer customer;
 
+    private class ListNode {
+        MenuItem value;
+        ListNode next;
+
+        ListNode(MenuItem value, ListNode next)
+        {
+            this.value=value;
+            this.next=next;
+        }
+    }
+
+    InternetOrder ()
+    {
+        head=null;
+    }
+
+    InternetOrder(MenuItem[] items, Customer customer)
+    {
+        this.items=items;
+        this.customer=customer;
+        this.size=items.length;
+    }
 }
