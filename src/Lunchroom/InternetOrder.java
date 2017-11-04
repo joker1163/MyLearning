f35da4448ae0e43509b08d1241679e9a041793e0
Joker1163 <slepushovii@gmail.com>
2017-11-04 09:44:38 +0400

    init commit

--- src/Lunchroom/InternetOrder.java
+++ src/Lunchroom/InternetOrder.java
@@ -29,4 +29,35 @@ public class InternetOrder implements Order {
         this.customer=customer;
         this.size=items.length;
     }
+
+
+    public boolean add(MenuItem item)
+    {
+        if(head==null)
+        {
+            head = new ListNode(item,null);
+            tail=head;
+        }
+        else
+        {
+            tail.next = new ListNode(item,null);
+            tail=tail.next;
+
+        }
+        ++size;
+        return true;
+    }
+    public boolean remove(String itemName);
+    public boolean remove(MenuItem item);
+    public int removeAll(String itemName);
+    public int removeAll(MenuItem item);
+    public int itemsQuantity();
+    public int itemQuantity(String itemName);
+    public int itemQuantity(MenuItem item);
+    public MenuItem[] getItems();
+    public double costTotal();
+    public String[] itemsNames();
+    public MenuItem[] sortedDishesByCostDesc();
+    public void setCustomer(Customer customer);
+    public Customer getCustomer();
 }
